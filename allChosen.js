f4852e804a70b077d13479e3af87994ef60cafff
leeY1ng <869500351@qq.com>
2017-06-06 10:59:26 +0800

    allChosen

---
+++ allChosen.js
@@ -0,0 +1,16 @@
+function checkboxed(objName) {
+	var objNameList = document.getElementsByName(objName);
+	if ( objNameList != null) {
+		for (var i = 0; i<objNameList.length; i++) {
+			objNameList[i].checked = "checked";
+		}
+	}
+}
+function uncheckboxed(objName) {
+	var objNameList = document.getElementsByName(objName);
+	if ( objNameList != null) {
+		for (var i = 0; i < objNameList.length; i++) {
+			objNameList[i].checked = "";
+		}
+	}
+}
